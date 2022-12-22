4ca646b97aebfff06b581c0448a1501c97fd5b7e
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-12-22 08:35:01 +0700

    chore: run prettier with yarn

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <h1 className=" text-red-500 text-3xl">Hello World</h1>
+      <h1 className="p-3 px-2 text-3xl text-red-500 sm:pt-2">Hello World</h1>
     </main>
   );
 }
