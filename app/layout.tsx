0c955f70647953c10060398a5d1d40b8c4462505
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-12-27 16:49:22 +0700

    feat: add next-seo and default seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Lora } from "@next/font/google";
 import { cn } from "#/lib/utils";
+import { NextSeo } from "next-seo";
 
 const lora = Lora({ subsets: ["latin"], variable: "--font-lora" });
 
@@ -11,7 +12,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cn(lora.variable, "[color-scheme:dark]")}>
-      <head />
+      <head>
+        {/* Used to be added by default, now we need to add manually */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        {/* 
+          Anything we add in layout will appear on EVERY PAGE. At present it can not be overridden lower down the tree.
+          This can be useful for things like favicons, or other meta tags that are the same on every page.
+        */}
+        <NextSeo
+          useAppDir={true}
+          themeColor="#73fa97"
+          titleTemplate="%s | Next SEO"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
