6e07c03d43d2ca93ae37c6a9fa88c8fe535d0077
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-12-27 13:50:57 +0700

    feat: setup jest

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    "^#/components/(.*)$": "<rootDir>/components/$1",
-    "^#/pages/(.*)$": "<rootDir>/pages/$1",
-    "^#/app/(.*)$": "<rootDir>/app/$1",
+    "^#/(.*)$": "<rootDir>/$1",
   },
   testEnvironment: "jest-environment-jsdom",
 };
